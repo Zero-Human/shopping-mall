82a3e21117887a5a0f58525129fea3d6111d3c1c
박주현 <leehot2665@naver.com>
2022-11-08 17:55:10 +0900

    Refactor : config 모듈 삭제 및 다른 모듈 domain 폴더로 이동

--- src/module/porducts/porducts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PorductsService } from './porducts.service';
-import { PorductsController } from './porducts.controller';
-
-@Module({
-  providers: [PorductsService],
-  controllers: [PorductsController]
-})
-export class PorductsModule {}
